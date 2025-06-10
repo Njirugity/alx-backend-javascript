044d7cc41acee646b95c20a8ed4e0fe2cf1f7f83
brian <muriithibrian283@gmail.com>
2025-06-10 21:25:49 +0300

    Add solutions to tasks 0 to 7

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,9 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Brian Njiru <https://github.com/Njirugity>
+ */
+const displayMessage = (message) => {
+    console.log(message)
+}
+module.exports = displayMessage
